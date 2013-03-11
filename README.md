4a24df3e5460b423605543ee2d7ed1ef3df09c2a
daveh86 <howsdav@gmail.com>
2013-03-11 15:11:49 +1100

    Initial commit of currentOp-digest

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ mongo-tools
 ===========
 
 Tools for working with MongoDB outputs
+
+### What's Here?
+* [currentOp-digest](README.md#currentop-digest)
+
+Allows you to digest the output of db.currentOp into a JSON one liner. Great for doing quick analysis.
+
+### Requirements
+ - Install the 'yajl-ruby' gem
+
+currentOp-digest
+---------------
+As it's name suggests this is for digesting the output of Mongo's db.currentOp true into a JSON one liner.
+Takes a single file as an argument and any number of the mix and match arguments listed below.
+Will automatically remove any leading data before the first open curly brace, but will fail if there is anything after the final curly brace. 
+
+Options are:
+These operations will each ouput seperately in batch after processing. If none are specified all are included
+* -s S ~ operations greater than S number of seconds running
+* -y Y ~ operations greater than Y number of yields 
+* -w W ~ operations holding write lock for more than W
+* -r R ~ operations holding read lock for more than R
+The following arguments can be added to help query out specific types of operations
+* -a ~ active operations only
+* -i ~ inactive operations only 
+* --ops [insert,query,getmore,update,none] ~ pick-and-mix list of valid operations
+* --opid X ~ operations that have the opid X
+* --ns NS ~ operations performed on namespace NS
+* --xns NS ~ operations not performed on namespace NS
+* --nsNotNill ~ operations performed on a namespace which does not evaluate to ""
+* --lim L ~ list only the top L operations for each of the initial batches
---
+++ currentOp-digest.rb
@@ -0,0 +1,242 @@
+#!/usr/bin/ruby
+
+require 'yajl'
+require 'optparse'
+
+#Init the Arrays
+
+readArray = Array.new()
+writeArray = Array.new()
+secsArray = Array.new()
+yieldsArray = Array.new()
+allArray = Array.new()
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: curropRun.rb [-r R|-w W| -s S] [-a|-f] filename"
+  opts.on("-w W", Integer, "Find write queries over W microseconds") do |v|
+    options[:write] = v.to_i
+  end
+  opts.on("-r R", Integer, "Find read queries over R microseconds locked") do |v|
+    options[:read] = v.to_i
+  end
+  opts.on("-s S", Integer, "Find queries over S seconds long") do |v|
+    options[:seconds] = v.to_i
+  end
+    opts.on("-y Y", Integer, "Find Queries yielded more than Y times") do |v|
+    options[:yield] = v
+  end
+  opts.on("-a", "Find active queries") do |v|
+    options[:active] = true
+  end
+  opts.on("-i", "Find in-active queries") do |v|
+    options[:active] = false
+  end
+  opts.on("--ops insert,query,getmore,update,none", Array, "Which operations?") do |v|
+    options[:ops] = v
+  end
+  opts.on("--opid opid", Integer, "Which operations?") do |v|
+    options[:opid] = v
+  end
+  opts.on("--ns ns", String, "Which Namespace?") do |v|
+    options[:ns] = v
+  end
+  opts.on("--xns ns", String, "Not which Namespace?") do |v|
+    options[:xns] = v
+  end
+    opts.on("--nsNotNill", "Not empty Namespaces?") do |v|
+    options[:nnns] = true
+  end
+  opts.on("--lim lim", Integer, "Display the top how many?") do |v|
+    options[:limit] = v
+  end
+end.parse!
+
+if ARGV[0] == NIL
+  puts "Usage: curropRun.rb [-r R|-w W| -s S] filename"
+  exit(1)
+end
+#p options
+#p ARGV
+
+
+def digestFile(filename)
+	fh = File.open(filename)
+	array =  String.new
+	preparse = true
+	fh.each_line do |line|
+		if preparse
+			if line =~ /\{/
+				preparse = false
+				array << line
+			end
+		else
+			#Sub numberlongs
+			if line.include? "NumberLong\("
+				line.slice!(/NumberLong\(/)
+				line.slice!(/\)/)
+			end
+
+			#ObjectId("4ef4af0963389003f300c2e7"),
+			if line.include? "ObjectId"
+				line = line.gsub("ObjectId\(\"", "\"ObjectId\(")
+				line = line.gsub("\"\)", "\)\"")
+			end
+
+			#Timestamp(10000, 27),
+			if line.include? ": Timestamp\("
+				line = line.gsub("Timestamp\(", "\"Timestamp\(")
+				line = line.gsub("\)", "\)\"")
+			end
+			#ISODate("2012-01-26T00:00:00Z")
+			if line.include? ": ISODate\(\""
+				line = line.gsub("ISODate\(\"", "\"ISODate\(")
+				line = line.gsub("\"\)", "\)\"")
+			end
+      #BinData
+      if line.include? ": BinData\("
+        line = line.gsub("BinData\(", "\"BinData\(")
+        line = line.gsub("\"\)", "\)\"")
+        line = line.gsub(",\"", ",")
+      end
+      if line.include? "\" : \/"
+        line = line.gsub("\" : \/", "\" : \"\/")
+        line = line.gsub("\/,", "\/\",")
+      end
+			if line !~ /bye/
+			  array << line
+			end
+		end
+	end
+	fh.close
+	doc = Yajl::Parser.parse(array)
+	return doc
+end
+
+###MAIN####
+object = digestFile(ARGV[0])["inprog"]
+
+object.each do |key|
+  #Remove Active or Inactive
+  if options[:active] != NIL
+    if options[:active]
+      if key["active"].equal? false
+        next
+      end
+    else
+      if key["active"].equal? true
+        next
+      end
+    end
+  end
+  #Check if a wanted Op type
+  if options[:ops] != NIL
+    if ! options[:ops].include? key["op"]
+      next
+    end
+  end
+  #Check if a wanted Opid
+  #puts "comparing #{options[:opid]}"
+  if options[:opid] != NIL
+    if options[:opid] != key["opid"]
+      next
+    end
+  end
+  #Check if a wanted NS
+  if options[:ns] != NIL
+    if ! (options[:ns] == key["ns"])
+      next
+    end
+  end
+  #Check if an unwanted NS
+  if options[:xns] != NIL
+    if options[:xns] == key["ns"]
+      next
+    end
+  end
+  #Check if a nil NS
+  if options[:nnns] == true
+    if key["ns"] == ""
+      next
+    end
+  end
+  #Find Write Locks
+  if options[:write] != NIL
+	  if key.has_key? "lockStats"
+      if key["lockStats"]
+        #Find Write Locks
+        if key["lockStats"]["timeLockedMicros"].has_key? "w"
+          if key["lockStats"]["timeLockedMicros"]["w"] > options[:write]
+            writeArray << key
+          end
+        end
+        if key["lockStats"]["timeLockedMicros"].has_key? "W"
+          if key["lockStats"]["timeLockedMicros"]["W"] > options[:write]
+            writeArray << key
+          end
+        end
+        if key["lockStats"]["timeLockedMicros"].has_key? "write"
+          if key["lockStats"]["timeLockedMicros"]["write"] > options[:write]
+            writeArray << key
+          end
+        end
+      end
+    end
+  end
+    #Find Read Locks
+  if options[:read] != NIL
+    if key["lockStats"]
+      #End Find Write Locks
+      if key["lockStats"]["timeLockedMicros"].has_key? "r"
+        if key["lockStats"]["timeLockedMicros"]["r"] > options[:read]
+          readArray << key
+        end
+      end
+      if key["lockStats"]["timeLockedMicros"].has_key? "R"
+        if key["lockStats"]["timeLockedMicros"]["R"] > options[:read]
+          readArray << key
+        end
+      end
+      if key["lockStats"]["timeLockedMicros"].has_key? "read"
+        if key["lockStats"]["timeLockedMicros"]["read"] > options[:read]
+          readArray << key
+        end
+      end
+    end
+  end
+  #Find Seconds running
+  if options[:seconds] != NIL
+    if key.has_key? "secs_running"
+      if key["secs_running"] > options[:seconds]
+        secsArray << key
+      end
+    end
+  end
+  if options[:yield] != NIL
+    if key.has_key? "numYields"
+      if key["numYields"] > options[:yield]
+        yieldsArray << key
+      end
+    end
+  end
+  if options[:seconds] == NIL && options[:read] == NIL && options[:write] == NIL && options[:yield] == NIL
+    allArray << key
+  end
+end
+
+
+if options[:read] != NIL
+  puts readArray.sort_by { |k| k["lockStats"]["timeLockedMicros"]["r"] }
+end
+if options[:write] != NIL
+  puts writeArray.sort_by { |k| k["lockStats"]["timeLockedMicros"]["w"] }
+end
+if options[:seconds] != NIL
+  puts secsArray.sort_by { |k| k["secs_running"] }
+end
+if options[:yield] != NIL
+  puts yieldsArray.sort_by { |k| k["numYields"] }
+end
+if options[:seconds] == NIL && options[:read] == NIL && options[:write] == NIL && options[:yield] == NIL
+  puts allArray
+end
